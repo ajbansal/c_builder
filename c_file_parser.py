4c1fb14207c7b57ba307f76eb9c071c0ee1f6b59
abhijit.bansal <abhijit.aj@gmail.com>
2017-12-31 00:32:04 -0800

    - Basic writing of py file

--- c_file_parser.py
+++ c_file_parser.py
@@ -20,6 +20,7 @@ Author:
 # region GLOBAL IMPORTS
 
 import logging
+from io import TextIOWrapper
 
 # endregion
 # **************************
@@ -38,14 +39,74 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-class CFileWriter(object):
-    def __init__(self, name):
+class PyFileWriter(object):
+    def __init__(self, *args, **kwargs):
+        self.fp = open(*args, **kwargs)
+
+    def write_var(self, var_name, var_value, indent=""):
+        if isinstance(var_value, str):
+            var_value = '"{var_value}"'.format(**locals())
+        self.fp.write("{indent}{var_name} = {var_value}\n".format(**locals()))
+
+    def write(self, text, indent=""):
+        if text[-1] != "\n":
+            text += "\n"
+
+        self.fp.write("{indent}{text}".format(**locals()))
+
+    def writelines(self, *args, **kwargs):
+        self.fp.writelines(*args, **kwargs)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """To be used with context managers"""
+        self.fp.close()
+
+    def __enter__(self):
+        """For use with context managers"""
+        return self
+
+    def __str__(self):
+        return "Instance of PyFile"
+
+    def __repr__(self):
+        return "Instance of PyFile"
+
+
+class CToPyFileConverter(object):
+    def __init__(self, name, output_dir, base_indent=" " * 4):
+
         self.name = name
+        self.out_dir = output_dir
+        self.base_indent = base_indent
+
         self.code_block = dict()
         self.data = []
 
     def render(self):
-        pass
+        py_file_name = "{self.name}_gen.py".format(**locals())
+        with PyFileWriter(py_file_name, 'w') as f:
+            f.write("from c_file_writer import *\n\n")
+            f.write_var("filename", "{self.name}_autogen.c".format(**locals()))
+            f.write_var("outputpath", self.out_dir)
+            f.write_var("indent", self.base_indent)
+            f.write("with CFile(filename, outputpath, indent) as f:\n")
+
+            # Write the data
+            for item in self.data:
+                if isinstance(item, str):
+                    # Simple lines
+
+                    if item.find(r"\n") not in [-1, len(item) - 1, 0]:
+                        # new line char exists somewhere in the middle
+                        # possibly as a printf statement
+                        py_line = r'f.write(r"""{item}""")'.format(**locals())
+                        f.write(py_line, self.base_indent)
+                        f.write(r'f.write("\n")', self.base_indent)
+                    else:
+                        py_line = r'f.write("""{item}\n""")'.format(**locals())
+                        f.write(py_line, self.base_indent)
+                else:
+                    logger.warning("NotImplemented for type - {}".format(type(item)))
 
     def add_line(self, value):
         self.data.append(value)
@@ -97,11 +158,16 @@ def parse_file(file_path):
     file_name, file_ext = os.path.splitext(os.path.basename(file_path))
 
     # Initialize python writer
-    writer = CFileWriter(file_name)
+    writer = CToPyFileConverter(file_name, os.path.dirname(file_path))
 
     for line in formatted_file:
         # logger.info(line)
-        print(line)
+        writer.add_line(line)
+
+    writer.render()
+
+    # For testing and comparison
+    # TODO: delete this code after
     with open("test.c", 'w') as f:
         f.write("\n".join(formatted_file))
 
--- c_test.c
+++ c_test.c
@@ -10,7 +10,7 @@
 #include<string.h>
 
 typedef struct {
-    
+
     char before;
     union {
     unsigned char byte[3];
@@ -22,16 +22,16 @@ typedef struct {
         uint32_t d : 4;
         uint32_t e : 4;
         uint32_t f : 4;
-        
+
     } field __attribute__((__packed__));
     } data;
     char after;
-    
+
 } bytes_3;
 
 
 typedef struct {
-    
+
     char before;
     union {
     unsigned char byte[4];
@@ -44,19 +44,19 @@ typedef struct {
         uint32_t d : 4;
         uint32_t e : 4;
         uint32_t f : 4;
-        
+
     } field __attribute__((__packed__));
     } data;
     char after;
-    
+
 } bytes_3_reserved;
 
 
 
-#pragma scalar_storage_order big-endian    
+#pragma scalar_storage_order big-endian
 
 typedef struct {
-    
+
     char before;
     union {
     unsigned char byte[3];
@@ -68,11 +68,11 @@ typedef struct {
         uint32_t c : 4;
         uint32_t b : 4;
         uint32_t a : 4;
-        
+
     } field __attribute__((__packed__));
     } data;
     char after;
-    
+
 } bytes_3Big;
 
 
@@ -85,10 +85,10 @@ struct TsDBCP_CSM1_Current_battery
     {
         struct
         {
-            uint64_t  current_CSM1 : 32u; //lint !e46 Invalid Lint errors in C99 compiler.  
-            uint64_t  errorType_CSM1 : 7u; //lint !e46 Invalid Lint errors in C99 compiler.  
-            uint64_t  errorIndication_CSM1 : 1u; //lint !e46 Invalid Lint errors in C99 compiler.  
-            uint64_t  currentUnfiltered_CSM1 : 24u; //lint !e46 Invalid Lint errors in C99 compiler.  
+            uint64_t  current_CSM1 : 32u; //lint !e46 Invalid Lint errors in C99 compiler.
+            uint64_t  errorType_CSM1 : 7u; //lint !e46 Invalid Lint errors in C99 compiler.
+            uint64_t  errorIndication_CSM1 : 1u; //lint !e46 Invalid Lint errors in C99 compiler.
+            uint64_t  currentUnfiltered_CSM1 : 24u; //lint !e46 Invalid Lint errors in C99 compiler.
         } signals; //!< Represents the signals in the message.
         uint8_t bytes[8];//lint !e46 Invalid Lint errors in C99 compiler. Raw data in one byte segments.
         uint64_t u64;
@@ -102,18 +102,18 @@ struct TsDBCP_CSM1_Current_battery
 
 // notice this is the same, except opposite order of bitfields
 
-struct TsDBCP_CSM2_Current_battery 
+struct TsDBCP_CSM2_Current_battery
 {
     uint32_t timestamp; //!< Used to determine MIA (missing in action) signals.
     union
     {
         struct
         {
-            
-            uint64_t  currentUnfiltered_CSM1 : 24u; //lint !e46 Invalid Lint errors in C99 compiler.  
-            uint64_t  errorIndication_CSM1 : 1u; //lint !e46 Invalid Lint errors in C99 compiler.  
-            uint64_t  errorType_CSM1 : 7u; //lint !e46 Invalid Lint errors in C99 compiler.  
-            uint64_t  current_CSM1 : 32u; //lint !e46 Invalid Lint errors in C99 compiler.  
+
+            uint64_t  currentUnfiltered_CSM1 : 24u; //lint !e46 Invalid Lint errors in C99 compiler.
+            uint64_t  errorIndication_CSM1 : 1u; //lint !e46 Invalid Lint errors in C99 compiler.
+            uint64_t  errorType_CSM1 : 7u; //lint !e46 Invalid Lint errors in C99 compiler.
+            uint64_t  current_CSM1 : 32u; //lint !e46 Invalid Lint errors in C99 compiler.
         } signals; //!< Represents the signals in the message.
         uint8_t bytes[8];//lint !e46 Invalid Lint errors in C99 compiler. Raw data in one byte segments.
         uint64_t u64;
@@ -127,13 +127,13 @@ void print (uint8_t *data)
     uint8_t i;
     for (i = 0; i<8; i++)
         printf("%02x ",data[i]);
-    printf("\n");   
+    printf("\n");
 }
 
 /** Function to set signal raw value in the message based on signal's start bit **/
 void setRawSignal(uint64_t rawValue, uint8_t *data, uint8_t startBit, int old)
 {
-    
+
     uint64_t data_cpy = 0;
     uint8_t i;
     //TODO figure out a replacement for memcpy
@@ -147,13 +147,13 @@ void setRawSignal(uint64_t rawValue, uint8_t *data, uint8_t startBit, int old)
         {
             data_cpy += ((uint64_t)data[i])<<(8*(7-i));
         }
-    }        
-    
-    rawValue = (rawValue << startBit);   
-    data_cpy |= rawValue;   
+    }
+
+    rawValue = (rawValue << startBit);
+    data_cpy |= rawValue;
     if (old)
     {
-        memcpy(data, &data_cpy, sizeof(data_cpy));    
+        memcpy(data, &data_cpy, sizeof(data_cpy));
     }
     else
     {
@@ -161,7 +161,7 @@ void setRawSignal(uint64_t rawValue, uint8_t *data, uint8_t startBit, int old)
         {
             data[i] = data_cpy >> (8*(7-i));
         }
-    }           
+    }
 }
 
 void test(void)
@@ -173,7 +173,7 @@ void test(void)
     //frame.DLC = 8u;
     //frame.Identifier = 961u;
     //Setting raw value of signal based on start bit
-    
+
     int old = 0;
     setRawSignal(0xABCDEF, src, 0, old);   //reserved_CSM2 (size 24)
     print (src);
@@ -183,20 +183,20 @@ void test(void)
     print (src);
     setRawSignal(0x01020304, src, 32, old);   //iString_CSM2 (size 32)
     print (src);
-    
+
     struct TsDBCP_CSM2_Current_battery littleEndian;
     struct TsDBCP_CSM1_Current_battery bigEndian;
     for (int i = 0; i<8; i++)
     {
-        littleEndian.data.bytes[i] = src[7-i];        
-        bigEndian.data.bytes[i] = src[i];        
+        littleEndian.data.bytes[i] = src[7-i];
+        bigEndian.data.bytes[i] = src[i];
 
         printf("%02x",littleEndian.data.bytes[i]);
     }
     printf("\n");
     printf("u64 little:\n%016llx\n", littleEndian.data.u64);
     printf("u64 big:\n%016llx\n", bigEndian.data.u64);
-    
+
     uint8_t *ptr;
     ptr = &littleEndian.data;
     printf("mem little:\n");
@@ -214,27 +214,27 @@ void test(void)
         ptr++;
     }
     printf("\n");
-    
-    
+
+
     printf("\n");
     printf("\n");
 
     printf("Little Endian\n");
     printf("%x\n",littleEndian.data.signals.current_CSM1);
     printf("%x\n",littleEndian.data.signals.errorType_CSM1);
-    printf("%x\n",littleEndian.data.signals.errorIndication_CSM1);  
+    printf("%x\n",littleEndian.data.signals.errorIndication_CSM1);
     printf("%x\n",littleEndian.data.signals.currentUnfiltered_CSM1);
     printf("\n");
-    
+
     printf("Big Endian\n");
     printf("%x\n",bigEndian.data.signals.current_CSM1);
     printf("%x\n",bigEndian.data.signals.errorType_CSM1);
-    printf("%x\n",bigEndian.data.signals.errorIndication_CSM1);  
+    printf("%x\n",bigEndian.data.signals.errorIndication_CSM1);
     printf("%x\n",bigEndian.data.signals.currentUnfiltered_CSM1);
     printf("\n");
-    
-    
-    
+
+
+
     bytes_3_reserved v3_reserved;
     v3_reserved.data.byte[0]=0x00;
     v3_reserved.data.byte[1]= 0x56;
@@ -242,42 +242,42 @@ void test(void)
     v3_reserved.data.byte[3]= 0x12;
     v3_reserved.before = 0xAA;
     v3_reserved.after = 0x55;
-    
-    
+
+
     printf("%x\n",v3_reserved.data.value);
     printf("%x\n",v3_reserved.data.field.a);
     printf("%x\n",v3_reserved.data.field.b);
     printf("%x\n",v3_reserved.data.field.f);
-    
-    
+
+
     bytes_3 v3;
     v3.data.byte[0]= 0x56;
     v3.data.byte[1]= 0x34;
     v3.data.byte[2]= 0x12;
     v3.before = 0xAA;
     v3.after = 0x55;
-    
-    
+
+
     printf("%x\n",v3.data.value << (8*(sizeof(v3.data) - sizeof(v3.data.byte))));
     printf("%x\n",v3.data.field.a);
     printf("%x\n",v3.data.field.b);
     printf("%x\n",v3.data.field.f);
-    
-    
+
+
     bytes_3Big v4;
     v4.data.byte[0]= 0x12;
     v4.data.byte[1]= 0x34;
     v4.data.byte[2]= 0x56;
     v4.before = 0xAA;
     v4.after = 0x55;
-    
-    
+
+
     printf("%x\n",v4.data.value);
     printf("%x\n",v4.data.field.a);
     printf("%x\n",v4.data.field.b);
     printf("%x\n",v4.data.field.f);
-    
-       
+
+
 }
 
 //===========Main function===========
